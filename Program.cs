f80afb6a9667b93c245011ef53b8ccbc1ac703dd
YongZhang <yong_zhang6@hnacapital.com>
2017-02-17 16:37:09 +0800

    update

--- Program.cs
+++ Program.cs
@@ -16,15 +16,15 @@ namespace mylc
             int pivot = nums[l];
             int low = l;
             int high = h;
-            
+
             while (low < high)
-            {                
-                while(low < high && nums[high] >= pivot)
+            {
+                while (low < high && nums[high] >= pivot)
                 {
                     high--;
                 }
 
-                if(low < high)
+                if (low < high)
                 {
                     nums[low] = nums[high];
                     low++;
@@ -34,12 +34,12 @@ namespace mylc
                     break;
                 }
 
-                while(low < high && nums[low] < pivot)
+                while (low < high && nums[low] < pivot)
                 {
                     low++;
                 }
 
-                if(low < high)
+                if (low < high)
                 {
                     nums[high] = nums[low];
                     high--;
@@ -73,24 +73,51 @@ namespace mylc
             //    Console.Write("{0} ", A[i]);
             //}
 
-            for (int i = 0; i < A.Length; i++)
+            //for (int i = 0; i < A.Length; i++)
+            //{
+            //    while (A[i] != i + 1 && A[i] != -1)
+            //    {
+            //        int index = A[i] - 1;
+            //        if (A[index] == A[i])
+            //        {
+            //            A[i] = -1;
+            //            Console.Write(A[index]);
+            //        }
+            //        else
+            //        {
+            //            A[i] = A[index];
+            //            A[index] = index + 1;
+            //        }
+            //    }
+            //}
+
+
+            string inputString = Console.ReadLine(); ;
+            int[,] Matrix = new int[inputString.Length, inputString.Length];
+            int startIndex = 0, endIndex = 0, max = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
             {
-                while (A[i] != i + 1 && A[i] != -1)
+                for (int j = i; j < inputString.Length; j++)
                 {
-                    int index = A[i] - 1;
-                    if (A[index] == A[i])
+                    if (i == 0)
+                        Matrix[j - i, j] = 1;
+                    else if (inputString[j - i] == inputString[j] && Matrix[j - i + 1, j - 1] != 0)
+                        Matrix[j - i, j] = Matrix[j - i + 1, j - 1] + 2;
+
+                    if (Matrix[j - i, j] > max)
                     {
-                        A[i] = -1;
-                        Console.Write(A[index]);
-                    }
-                    else
-                    {
-                        A[i] = A[index];
-                        A[index] = index + 1;
+                        startIndex = j - i;
+                        endIndex = j;
+                        max = Matrix[j - i, j];
                     }
                 }
             }
+
+            for (int i = startIndex; i <= endIndex; i++)
+                Console.Write(inputString[i]);
+
             Console.ReadKey();
-        }
+        } 
     }
 }
